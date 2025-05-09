b24de2deff1df9838a53bfbf1c68ab0a81e9ccc8
kostys <kostyabogdanov338@gmail.com>
2025-05-09 07:45:44 +0300

    refactor chat config

--- support.core/src/main/kotlin/ai/support/demo/config/ChatClientConfig.kt
+++ support.core/src/main/kotlin/ai/support/demo/config/ChatClientConfig.kt
@@ -1,33 +1,64 @@
 package ai.support.demo.config
 
 import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.model.ChatModel
+import org.springframework.ai.embedding.EmbeddingModel
 import org.springframework.ai.ollama.OllamaChatModel
+import org.springframework.ai.ollama.OllamaEmbeddingModel
 import org.springframework.ai.ollama.api.OllamaApi
-import org.springframework.ai.ollama.api.OllamaModel
 import org.springframework.ai.ollama.api.OllamaOptions
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class ChatClientConfig {
 
-    @Bean
-    fun chatClient() : ChatClient {
+    @Value("\${ollama.url}")
+    lateinit var baseUrl: String;
 
-        val api = OllamaApi.builder()
-            .baseUrl("http://localhost:11434")
-            .build()
-        val chatModel = OllamaChatModel.builder()
+    @Value("\${ollama.model.chat}")
+    lateinit var chatModel: String;
+
+    @Value("\${ollama.model.embedding}")
+    lateinit var embeddingModel: String;
+
+    @Bean
+    fun chatClient(chatModel: ChatModel): ChatClient {
+        return ChatClient.create(chatModel)
+    }
+
+    @Bean
+    fun embeddingModel(api: OllamaApi): EmbeddingModel {
+        return OllamaEmbeddingModel.builder()
             .ollamaApi(api)
             .defaultOptions(
                 OllamaOptions.builder()
-                    .model(OllamaModel.LLAMA3_2_1B)
+                    .model(embeddingModel)
+                    .truncate(false)
+                    .build()
+            )
+            .build()
+    }
+
+    @Bean
+    fun chatModel(api: OllamaApi): ChatModel {
+        return OllamaChatModel.builder()
+            .ollamaApi(api)
+            .defaultOptions(
+                OllamaOptions.builder()
+                    .model(chatModel)
                     .temperature(0.9)
                     .build()
             )
             .build();
+    }
 
-        return ChatClient.create(chatModel)
+    @Bean
+    fun modelApi(): OllamaApi {
+        return OllamaApi.builder()
+            .baseUrl(baseUrl)
+            .build();
     }
 
 }
